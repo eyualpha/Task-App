e68aeae5d9cb102f5731aed0964ec5e10d2323cd
eyualpha <eyuashenafi533@gmail.com>
2026-01-27 00:16:12 +0300

    feat: implement tab navigation with tasks and settings screens

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,31 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+
+const TabLayout = () => {
+  return (
+    <Tabs screenOptions={{ headerShown: false, tabBarActiveTintColor: "red" }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Tasks",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="flash-outline" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: "Settings",
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="settings-outline" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+};
+
+export default TabLayout;
