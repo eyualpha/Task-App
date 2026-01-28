e2cd482890d098ba95bb4d5079fbd7b23cb51693
eyualpha <eyuashenafi533@gmail.com>
2026-01-28 23:01:56 +0300

    feat: integrate Convex provider and add task management functionality in index screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,14 @@
+import { api } from "@/convex/_generated/api";
 import useTheme from "@/hooks/useTheme";
+import { useMutation, useQuery } from "convex/react";
 import { Link } from "expo-router";
 import { Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   const { toggleDarkMode } = useTheme();
+  const todos = useQuery(api.tasks.getTasks);
+  const addTodo = useMutation(api.tasks.addTask);
+  console.log(todos);
   return (
     <View
       style={{
@@ -15,6 +20,12 @@ export default function Index() {
       <Text>Edit app/index.tsx to edit this screen.</Text>
       <TouchableOpacity style={{ marginTop: 20 }} onPress={toggleDarkMode}>
         <Text>Toggle Theme</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={{ marginTop: 20 }}
+        onPress={() => addTodo({ title: "New todo" })}
+      >
+        <Text>Add todo</Text>
       </TouchableOpacity>
     </View>
   );
